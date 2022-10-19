b77d7882a10daee88b11863f7c376d9f240448e5
Tamunokuro <tamunokrobluejack@gmail.com>
2022-10-19 14:44:07 +0100

    Update mobile view with HTML tags and CSS styles

--- README.md
+++ README.md
@@ -1,15 +1,14 @@
 ![](https://img.shields.io/badge/Microverse-blueviolet)
 
-# Project Name
+# Drummers Summit
 
-> Description the project.
+> This is a personal portfolio project for a Music Fest referred to as the drummers summit
 
 
 ## Built With
 
-- Major languages
-- Frameworks
-- Technologies used
+- HTML, CSS, JavaScirpt
+- GIT 
 
 ## Live Demo (if available)
 
@@ -18,39 +17,23 @@
 
 ## Getting Started
 
-**This is an example of how you may give instructions on setting up your project locally.**
-**Modify this file to match your project, remove sections that don't apply. For example: delete the testing section if the currect project doesn't require testing.**
-
-
 To get a local copy up and running follow these simple example steps.
 
+
 ### Prerequisites
+- Install GIT
 
 ### Setup
-
-### Install
-
-### Usage
-
-### Run tests
-
-### Deployment
-
+- Run git clone 
 
 
 ## Authors
 
 👤 **Author1**
 
-- GitHub: [@githubhandle](https://github.com/githubhandle)
-- Twitter: [@twitterhandle](https://twitter.com/twitterhandle)
-- LinkedIn: [LinkedIn](https://linkedin.com/in/linkedinhandle)
-
-👤 **Author2**
-
-- GitHub: [@githubhandle](https://github.com/githubhandle)
-- Twitter: [@twitterhandle](https://twitter.com/twitterhandle)
-- LinkedIn: [LinkedIn](https://linkedin.com/in/linkedinhandle)
+- GitHub: [@githubhandle](https://github.com/Tamunokuro)
+- Twitter: [@twitterhandle](https://twitter.com/tamuno___)
+- LinkedIn: [LinkedIn](https://linkedin.com/in/joshua-blue-jack)
 
 ## 🤝 Contributing
 
@@ -64,9 +47,8 @@ Give a ⭐️ if you like this project!
 
 ## Acknowledgments
 
-- Hat tip to anyone whose code was used
-- Inspiration
-- etc
+- Design by [Cindy Shin](https://www.behance.net/adagio07)
+- Photos from [Unsplash](https://unsplash.com/)
 
 ## 📝 License
 
---
+++ js/script.js
@@ -0,0 +1,75 @@
+const speaker = document.getElementById('speakers');
+const humburger = document.querySelector('.mobile-menu-bar');
+const navmenu = document.querySelector('.desktop-menu');
+const menu = document.querySelector('.desktop-menu');
+const body = document.querySelector('body');
+const link = document.querySelectorAll('.nav-link')
+
+const speakerObj = [
+    { 
+      name: 'Emily Brook',
+      image: './img/speaker-1.jpg',
+      role: 'Music Media Directior',
+      text: 'Emily is a successful media director',
+    },
+    {
+      name: 'Dante James',
+      image: './img/speaker-2.jpg',
+      role: 'Lead Drummer for Brooklyn Music',
+      text: 'Dante is a big drum lover with alot of experience',
+    },
+    {
+      name: 'Mason Bundy',
+      image: './img/speaker-3.jpg',
+      role: 'Sound Engineer',
+      text: 'Mason: I can make the sound come alive',
+    },
+    {
+      name: 'Aliyah Brown',
+      image: './img/speaker-4.jpg',
+      role: 'YAMAHA Drums Marketer Expert',
+      text: 'Aliyah can teach you more about purchasing drums',
+    },
+    {
+      name: 'Henry Barnes',
+      image: './img/speaker-5.jpg',
+      role: 'Drum Music Directior',
+      text: 'Let\s talk about drum rudiments!',
+    },
+    {
+      name: 'Mark Enze',
+      image: './img/speaker-6.jpg',
+      role: 'Lead Drummer:Maverrick City Music',
+      text: 'We can all be successful in playing the drums',
+    },
+]
+
+let presenter = '';
+speakerObj.forEach((child, index) => {
+  presenter = `
+  <div class="speaker">
+  <div class="speaker-photo"></div>
+  <img class="speaker-image" src="${child.image}" alt="image of speaker">
+  <div class="speaker-details">
+      <p class="speaker-name">${child.name}</p>
+      <p class="speaker-business">${child.role}</p>
+      <p>${child.text}</p>
+  </div>
+  </div>
+  `;
+  if (speaker != null) {
+    speaker.innerHTML += presenter;
+  }
+});
+
+humburger.addEventListener('click', () => {
+  menu.classList.toggle('active');
+  humburger.classList.toggle('active');
+});
+
+link.forEach((nav) => {
+  nav.addEventListener('click', () => {
+    navmenu.classList.toggle('active');
+    humburger.classList.toggle('active');
+  })
+})
